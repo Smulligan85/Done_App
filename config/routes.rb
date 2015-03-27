d60b9d9e38a032d492a21896d99c7f4936a4691b
Sean Mulligan <sean.mulligan85@gmail.com>
2015-03-26 18:46:54 -0700

    Fix sign in redirect to user#show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  
+
+  # authenticated :user do
+  #   root to: 'users#show', as: :authenticated_root
+  # end  
 
   devise_for :users
     resources :users, only: [:show]
+
+
   root to: 'welcome#index'
 
 end
